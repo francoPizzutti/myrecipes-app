9b84a0487c009770937d0a9da2fab4920d2bcf01
Franco Pizzutti <fpizzuttimed@gmail.com>
2023-11-08 15:22:11 -0300

    Improve system compose and provide database connection. Create user layer

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-
+import { userRouter } from './routes/users.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(3080, () => console.log('sv started')); 
+app.use('/auth', userRouter);
+
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+      authSource: 'admin', 
+      user: process.env.MONGO_INITDB_ROOT_USERNAME,
+      pass: process.env.MONGO_INITDB_ROOT_PASSWORD,
+      dbName: 'recipes'
+});
+
+app.listen(3001, () => console.log('sv started')); 
